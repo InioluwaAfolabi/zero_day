6c0ab59d829cb8c6f33d6094c715cc0605cf859a
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-12-22 08:09:55 -0800

    commit assignment operators

--- bitw.c
+++ bitw.c
@@ -4,14 +4,19 @@ int main(void)
 {
 	int var = 3;
 	int a = 4, b = 5;
+	char s = 7;
 
+	s ^= 5;
 	a = a ^ b;
 	b = a ^ b;
 	a = a ^ b;
-
+	printf("The operation gave an answer of: %d\n", s);
 	printf("After XOR, a = %d and b = %d\n", a, b);
 
 	printf("%d\n", var << 1);
 	printf("%d\n", var >> 2);
+	a += 4;
+	printf("a = %d\n", a);
+
 	return (0);
 }
