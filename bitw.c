a3d76aa117e0fe09020693b967d09b039e791cbc
InioluwaAfolabi <afolabiinioluwa504@gmail.com>
2022-12-22 07:53:51 -0800

    commit bitwisee

--- bitw.c
+++ bitw.c
@@ -3,7 +3,15 @@
 int main(void)
 {
 	int var = 3;
+	int a = 4, b = 5;
+
+	a = a ^ b;
+	b = a ^ b;
+	a = a ^ b;
+
+	printf("After XOR, a = %d and b = %d\n", a, b);
 
 	printf("%d\n", var << 1);
+	printf("%d\n", var >> 2);
 	return (0);
 }
